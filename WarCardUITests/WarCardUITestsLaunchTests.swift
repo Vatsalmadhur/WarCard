e2eac885f088c4e303244f26acc121b73bcdc721
Madhur <bhartimadhur21@gmail.com>
2023-08-07 11:12:06 +0530

    Initial Commit

---
+++ WarCardUITests/WarCardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WarCardUITestsLaunchTests.swift
+//  WarCardUITests
+//
+//  Created by Madhur Vatsal Bharti on 07/08/23.
+//
+
+import XCTest
+
+final class WarCardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
